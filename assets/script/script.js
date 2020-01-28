c80bffcf40a93f08e659a0b48e2ea733e5f1e46c
7-Spades <edwin5106@gmail.com>
2020-01-27 22:17:27 -0600

    added script that saves and retieves written input

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,29 @@
 console.log($)
+console.log(moment);
+
 $(document).ready(function(){
-    
+    var stamp= $("#timestamper");
+    var record= $("#recorder");
+    var saver= $(".saveState");
+
+    stamp.text(moment().format(" h:mm a, MMMM Do YYYY"));
+
+    displayrecord();
+
+    saver.on("click", function(event){
+        event.preventDefault();
+        var rec= record.val();
+        
+        localStorage.setItem("Agenda", rec);
+        displayrecord();
+    });
+
+    $("form").on("submit", function(event){
+        event.preventDefault();
+    });
+
+    function displayrecord(){
+    record.attr("value", localStorage.getItem("Agenda"))
+    };
 
 });
